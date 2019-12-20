76b9e1257a3abbbce215a605f157ecd179fb2c28
sakorn.s <sakorn.s@github.com>
2019-12-20 15:28:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <!-- Title -->
+        <title>Services 1 | | Unify - Responsive Website Template</title>
+
+        <!-- Required Meta Tags Always Come First -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="favicon.ico">
+
+        <!-- CSS Global Compulsory -->
+        <link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
+        <!-- CSS Global Icons -->
+        <link rel="stylesheet" href="assets/vendor/icon-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/vendor/icon-line/css/simple-line-icons.css">
+        <link rel="stylesheet" href="assets/vendor/icon-etlinefont/style.css">
+        <link rel="stylesheet" href="assets/vendor/icon-line-pro/style.css">
+        <link rel="stylesheet" href="assets/vendor/icon-hs/style.css">
+        <link rel="stylesheet" href="assets/vendor/animate.css">
+        <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsparallaxer.css">
+        <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
+        <link rel="stylesheet" href="assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
+        <link rel="stylesheet" href="assets/vendor/fancybox/jquery.fancybox.css">
+        <link rel="stylesheet" href="assets/vendor/hs-megamenu/src/hs.megamenu.css">
+        <link rel="stylesheet" href="assets/vendor/hamburgers/hamburgers.min.css">
+
+        <!-- CSS Unify -->
+        <link rel="stylesheet" href="assets/css/unify-core.css">
+        <link rel="stylesheet" href="assets/css/unify-components.css">
+        <link rel="stylesheet" href="assets/css/unify-globals.css">
+
+        <!-- CSS Customization -->
+        <link rel="stylesheet" href="assets/css/custom.css">
+        <link rel="stylesheet" href="css/font-kanit.css">
+    </head>
+    <?php
+    // $url =  "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+    $url = $url = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    ?>
+
+    <body>
+        <main>
+
+            <!-- Section -->
+            <section class="container g-pt-50 g-pb-40">
+                <div class="row">
+                    <div class="col-lg-6 g-mb-60">
+                        <div class="mb-4">
+                            <h2 class="h3 text-uppercase mb-3">วิตามินกระตุ้นรากผม Multivitamin</h2>
+                            <div class="g-width-60 g-height-1 g-bg-black"></div>
+                        </div>
+                        <div class="mb-5">
+
+                            <p>“การรักษาผมร่วง ผมบาง ศรีษะล้าน ที่ได้ผลดีที่สุดคือ การรักษาหรือยืดอายุเซลล์รากผมไว้ให้ได้นานที่สุด ไม่ให้มันถูกทำลายจนหมด” โดยวิธีนี้จำเป็นต้องได้รับการรักษาแต่เนิ่นๆ ตั้งแต่มองเห็นอาการ</p>
+                            <p>Multivitamin ไม่มีผลข้างเคียงใดๆต่อร่างกาย เนื่องจากเป็นสารสกัดจากธรรมชาติและเป็นวิตามินกลุ่มที่ละลายในน้ำ ไม่ละลายในไขมัน ร่างกายจึงสามารถขับวิตามินส่วนเกินออกจากร่างกายได้ง่าย โดยไม่สะสมในตับ</p>
+                        </div>
+                        <a class="btn btn-xl u-btn-outline-black g-font-size-default mr-2" href="#!">ปรึกษาฟรี!!</a>
+                        <a class="btn btn-xl u-btn-primary g-font-size-default" href="#!">สั่งซื้อสินค้า</a>
+                    </div>
+
+                    <div class="col-lg-6 g-mb-60">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/haQsDv9ixUI?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- End Section -->
+
+            <!-- Hero Info -->
+            <section class="dzsparallaxer auto-init height-is-based-on-content use-loading" data-options='{direction: "reverse", settings_mode_oneelement_max_offset: "150"}'>
+                <!-- Parallax Image -->
+                <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-black-opacity-0_7--after" style="height: 140%; background-image: url(<?= $url ?>img/1920X1080.jpg)"></div>
+                <!-- End Parallax Image -->
+
+                <div class="container text-center g-color-white g-py-150--md g-py-80">
+                    <div class="row">
+                        <div class="col-md-8 ml-md-auto mr-md-auto">
+
+                            <h2 class="h1 text-uppercase mb-3">จบทุกปัญหาผม ผลิตภัณฑ์โดยแพทย์ผู้เชี่ยวชาญ</h2>
+                            <div class="d-inline-block g-width-60 g-height-1 g-bg-white mb-3"></div>
+
+                            <p class="lead g-mb-60">"มัลติไวตามิน" ที่ HSC- Hair Solution Clinic เป็นสูตรเฉพาะ คัดสรรวัตถุดิบเกรดพรีเมี่ยมในการผลิต กระตุ้นเส้นผมให้ขึ้นมาใหม่ แข็งแรง เงางาม ไม่หลุดร่วงง่าย</p>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Hero Info -->
+            <section class="container g-pt-100 g-pb-40">
+                <div class="row">
+                    <div class="col-lg-6 align-self-center g-mb-60">
+                        <img class="img-fluid" src="img/ads.jpg" alt="Image Description">
+                    </div>
+
+                    <div class="col-lg-6 align-self-center g-mb-60">
+                        <div class="mb-2">
+                            <h2 class="h3 g-color-black text-uppercase mb-2">สรรพคุณของ Multivitamin</h2>
+                            <div class="d-inline-block g-width-35 g-height-2 g-bg-primary mb-2"></div>
+                        </div>
+                        <div class="mb-5">
+
+                            <ul class="list-unstyled g-font-size-17 mb-0">
+                                <li>
+                                    <i class="d-inline-block g-color-primary mr-2 mb-3 fa fa-check"></i>
+                                    หยุดผมร่วง
+                                </li>
+                                <li>
+                                    <i class="d-inline-block g-color-primary mr-2 mb-3 fa fa-check"></i>
+                                    กระตุ้นรากผมใหม่
+                                </li>
+                                <li>
+                                    <i class="d-inline-block g-color-primary mr-2 mb-3 fa fa-check"></i>
+                                    ชลอผมหงอก
+                                </li>
+                                <li>
+                                    <i class="d-inline-block g-color-primary mr-2 mb-3 fa fa-check"></i>
+                                    เส้นผมใหญ่ และเงางาม
+                                </li>
+                            </ul>
+                        </div>
+                        <a class="btn btn-md u-btn-outline-black g-font-size-default g-rounded-50 g-py-10 mr-2" href="#!">ปรึกษาฟรี!!</a>
+                        <a class="btn btn-md u-btn-primary g-font-size-default g-rounded-50 g-py-10" href="#!">สั่งซื้อสินค้า</a>
+                    </div>
+                </div>
+            </section>
+
+
+            <!-- Counters -->
+            <section class="dzsparallaxer auto-init height-is-based-on-content">
+                <!-- Parallax Image -->
+                <div class="dzsparallaxer--target w-100" style="height: 140%; background: url(assets/img/bg/pattern2.png)"></div>
+                <!-- End Parallax Image -->
+
+                <div class="container g-pt-100 g-pb-70">
+                    <div class="row text-center text-uppercase">
+                        <div class="col-md-6 col-lg-3 g-mb-30">
+                            <!-- Counters -->
+                            <div class="g-bg-white g-rounded-4 g-brd-bottom g-brd-2 g-brd-gray-light-v3 g-pa-20">
+                                <div class="js-counter h1 g-font-weight-300 g-mb-5">18298</div>
+                                <h4 class="g-font-weight-300">จำนวนลูกค้าของเรา</h4>
+                            </div>
+                        </div>
+                        <!-- End Counters -->
+
+                        <div class="col-md-6 col-lg-3 g-mb-30">
+                            <!-- Counters -->
+                            <div class="g-bg-white g-rounded-4 g-brd-bottom g-brd-2 g-brd-gray-light-v3 g-pa-20">
+                                <div class="js-counter h1 g-font-weight-300 g-mb-5">24583</div>
+                                <h4 class="g-font-weight-300">ลูกค้ากลับมาซื้อซ้ำ</h4>
+                            </div>
+                        </div>
+                        <!-- End Counters -->
+
+                        <div class="col-md-6 col-lg-3 g-mb-30">
+                            <!-- Counters -->
+                            <div class="g-bg-white g-rounded-4 g-brd-bottom g-brd-2 g-brd-gray-light-v3 g-pa-20">
+                                <div class="js-counter h1 g-font-weight-300 g-mb-5">37904</div>
+                                <h4 class="g-font-weight-300">ผู้แสดงความสนใจ</h4>
+                            </div>
+                        </div>
+                        <!-- End Counters -->
+
+                        <div class="col-md-6 col-lg-3 g-mb-30">
+                            <!-- Counters -->
+                            <div class="g-bg-white g-rounded-4 g-brd-bottom g-brd-2 g-brd-gray-light-v3 g-pa-20">
+                                <div class="js-counter h1 g-font-weight-300 g-mb-5">50892</div>
+                                <h4 class="g-font-weight-300">ลูกค้าโทรปรึกษาเรา</h4>
+                            </div>
+                        </div>
+                        <!-- End Counters -->
+                    </div>
+                </div>
+            </section>
+            <!-- End Counters -->
+            <section class="container g-py-100">
+                <div class="row justify-content-center g-mb-60">
+                    <div class="col-lg-7">
+                        <!-- Heading -->
+                        <div class="text-center">
+                            <h2 class="h3 text-uppercase mb-3">รีวิวจากลูกค้าของเรา 100%</h2>
+                            <div class="d-inline-block g-width-60 g-height-1 g-bg-black mb-2"></div>
+                           
+                        </div>
+                        <!-- End Heading -->
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/1.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/2.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/3.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/4.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/5.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/6.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/7.JPG"/>
+                    </div>
+                    <div class="col-md-6">
+                        <image class="img-fluid w-100" src="img/review/8.JPG"/>
+                    </div>
+                </div>
+
+            </section>
+
+            <!-- Copyright Footer -->
+            <footer class="g-bg-gray-dark-v1 g-color-white-opacity-0_8 g-py-20">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 text-center text-md-left g-mb-10 g-mb-0--md">
+                            <div class="d-lg-flex">
+                                <small class="d-block g-font-size-default g-mr-10 g-mb-10 g-mb-0--md">2018 &copy; All Rights Reserved.</small>
+                                <ul class="u-list-inline">
+                                    <li class="list-inline-item">
+                                        <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Privacy Policy</a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <span>|</span>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Terms of Use</a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <span>|</span>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">License</a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <span>|</span>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Support</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 align-self-center">
+                            <ul class="list-inline text-center text-md-right mb-0">
+                                <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Facebook">
+                                    <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Skype">
+                                    <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
+                                        <i class="fa fa-skype"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Linkedin">
+                                    <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Pinterest">
+                                    <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
+                                        <i class="fa fa-pinterest"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Twitter">
+                                    <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Dribbble">
+                                    <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
+                                        <i class="fa fa-dribbble"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- End Copyright Footer -->
+            <a class="js-go-to u-go-to-v1" href="#!" data-type="fixed" data-position='{
+               "bottom": 15,
+               "right": 15
+               }' data-offset-top="400" data-compensation="#js-header" data-show-effect="zoomIn">
+                <i class="hs-icon hs-icon-arrow-top"></i>
+            </a>
+        </main>
+
+        <div class="u-outer-spaces-helper"></div>
+
+
+        <!-- JS Global Compulsory -->
+        <script src="assets/vendor/jquery/jquery.min.js"></script>
+        <script src="assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
+        <script src="assets/vendor/popper.min.js"></script>
+        <script src="assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+
+        <!-- JS Implementing Plugins -->
+        <script src="assets/vendor/appear.js"></script>
+        <script src="assets/vendor/hs-megamenu/src/hs.megamenu.js"></script>
+        <script src="assets/vendor/dzsparallaxer/dzsparallaxer.js"></script>
+        <script src="assets/vendor/dzsparallaxer/dzsscroller/scroller.js"></script>
+        <script src="assets/vendor/dzsparallaxer/advancedscroller/plugin.js"></script>
+        <script src="assets/vendor/fancybox/jquery.fancybox.min.js"></script>
+
+        <!-- JS Unify -->
+        <script src="assets/js/hs.core.js"></script>
+        <script src="assets/js/components/hs.header.js"></script>
+        <script src="assets/js/helpers/hs.hamburgers.js"></script>
+        <script src="assets/js/components/hs.tabs.js"></script>
+        <script src="assets/js/components/hs.counter.js"></script>
+        <script src="assets/js/components/hs.popup.js"></script>
+        <script src="assets/js/components/hs.go-to.js"></script>
+
+        <!-- JS Customization -->
+        <script src="assets/js/custom.js"></script>
+
+        <!-- JS Plugins Init. -->
+        <script>
+            $(document).on('ready', function () {
+                // initialization of go to
+                $.HSCore.components.HSGoTo.init('.js-go-to');
+
+                // initialization of tabs
+                $.HSCore.components.HSTabs.init('[role="tablist"]');
+
+                // initialization of counters
+                var counters = $.HSCore.components.HSCounter.init('[class*="js-counter"]');
+
+                // initialization of popups
+                $.HSCore.components.HSPopup.init('.js-fancybox', {
+                    transitionEffect: false
+                });
+            });
+
+            $(window).on('load', function () {
+
+                
+              
+            });
+
+            $(window).on('resize', function () {
+                setTimeout(function () {
+                    $.HSCore.components.HSTabs.init('[role="tablist"]');
+                }, 200);
+            });
+            
+        </script>
+
+
+
+
+
+
+
+    </body>
+
+</html>
